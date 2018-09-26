dab04abc5bccb5515f061b5d85f451475136e4d6
Claudio5 <claudio.loureiro1@gmail.com>
2018-09-26 17:10:09 +0200

    Initial bootcamp app added

---
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/MainActivityTest.java
@@ -0,0 +1,31 @@
+package ch.epfl.sweng.SDP;
+
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public final ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void testCanGreetUsers() {
+        onView(ViewMatchers.withId(R.id.editMainText)).perform(typeText("from my unit test")).perform(closeSoftKeyboard());
+        onView(withId(R.id.mainGoButton)).perform(click());
+        onView(withId(R.id.greetingMessage)).check(matches(withText("Hello from my unit test!")));
+    }
+
+}
---
+++ app/src/main/java/ch/epfl/sweng/SDP/MainActivity.java
@@ -0,0 +1,23 @@
+package ch.epfl.sweng.SDP;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+
+        Intent intent = new Intent(this, GreetingActivity.class);
+        startActivity(intent);
+    }
+}

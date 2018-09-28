340009b2b84f8d3e9fc1ac76d758dff15cc44ca1
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-09-28 17:03:03 +0200

    Update MainActivity
    The button now starts LoginActivity and a placeholder for the logo has 
    been added

--- app/src/main/java/ch/epfl/sweng/SDP/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/SDP/MainActivity.java
@@ -13,11 +13,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        // Do something in response to button
-
-        Intent intent = new Intent(this, GreetingActivity.class);
+    /**
+     * Starts the {@link LoginActivity}.
+     * @param view the view corresponding to the clicked button
+     */
+    public void startLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }

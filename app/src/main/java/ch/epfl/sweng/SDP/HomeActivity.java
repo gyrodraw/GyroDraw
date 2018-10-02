ce8f0a747c5f8f9d7a989d11f1edccb45e38ddf3
LudoHoff <ludo.hoffstetter@epfl.ch>
2018-10-02 02:47:12 +0200

    Fixed every button animation (now react on press and release and the animation is smoother)

--- app/src/main/java/ch/epfl/sweng/SDP/HomeActivity.java
+++ app/src/main/java/ch/epfl/sweng/SDP/HomeActivity.java
@@ -3,6 +3,7 @@ package ch.epfl.sweng.SDP;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -22,40 +23,91 @@ public class HomeActivity extends AppCompatActivity {
         view.setBackgroundResource(R.color.colorGrey);
 
         Typeface typeMuroslant = Typeface.createFromAsset(getAssets(),"fonts/Muroslant.otf");
-
-        TextView leagueText = findViewById(R.id.leagueText);
+        Typeface typeMuro = Typeface.createFromAsset(getAssets(),"fonts/Muro.otf");
         Typeface typeOptimus = Typeface.createFromAsset(getAssets(),"fonts/Optimus.otf");
+
+        final Button drawButton = findViewById(R.id.drawButton);
+        final Button trophiesButton = findViewById(R.id.trophiesButton);
+        final Button starsButton = findViewById(R.id.starsButton);
+        final ImageView leagueImage = findViewById(R.id.leagueImage);
+        TextView leagueText = findViewById(R.id.leagueText);
+
+        drawButton.setTypeface(typeMuroslant);
+        drawButton.setPadding(0, -14, 0, 0);
+        drawButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        pressButton(drawButton);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        bounceButton(drawButton, 0.2, 20);
+                }
+                return false;
+            }
+        });
+
+        trophiesButton.setTypeface(typeMuro);
+        trophiesButton.setPadding(140, -5, 0, 0);
+        trophiesButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        pressButton(trophiesButton);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        bounceButton(trophiesButton, 0.1, 10);
+                }
+                return false;
+            }
+        });
+
+        starsButton.setTypeface(typeMuro);
+        starsButton.setPadding(140, -5, 0, 0);
+        starsButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        pressButton(starsButton);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        bounceButton(starsButton, 0.1, 10);
+                }
+                return false;
+            }
+        });
+
+        leagueImage.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        pressButton(leagueImage);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        bounceButton(leagueImage, 0.1, 30);
+                }
+                return true;
+            }
+        });
+
         leagueText.setTypeface(typeOptimus);
         leagueText.setPadding(0, -14, 0, 0);
-
-        TextView trophiesText = findViewById(R.id.trophiesButton);
-        trophiesText.setTypeface(typeMuroslant);
-        trophiesText.setPadding(60, -5, 0, 0);
-
-        TextView starsText = findViewById(R.id.starsButton);
-        starsText.setTypeface(typeMuroslant);
-        starsText.setPadding(60, -5, 0, 0);
     }
 
-    public void onClickTrophiesButton(View view) {
-        Button trophiesButton = findViewById(R.id.trophiesButton);
-        bounceButton(trophiesButton);
+    private void bounceButton(View view, double amplitude, int frequency) {
+        final Animation bounce = AnimationUtils.loadAnimation(this, R.anim.bounce);
+        BounceInterpolator interpolator = new BounceInterpolator(amplitude, frequency);
+        bounce.setInterpolator(interpolator);
+        view.startAnimation(bounce);
     }
 
-    public void onClickStarsButton(View view) {
-        Button starsButton = findViewById(R.id.starsButton);
-        bounceButton(starsButton);
-    }
-
-    public void onClickLeagueImage(View view) {
-        ImageView leagueImage = findViewById(R.id.leagueImage);
-        bounceButton(leagueImage);
-    }
-
-    private void bounceButton(View view) {
-        final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
-        BounceInterpolator interpolator = new BounceInterpolator(0.2, 20);
-        myAnim.setInterpolator(interpolator);
-        view.startAnimation(myAnim);
+    private void pressButton(View view) {
+        final Animation press = AnimationUtils.loadAnimation(this, R.anim.press);
+        press.setFillAfter(true);
+        view.startAnimation(press);
     }
 }

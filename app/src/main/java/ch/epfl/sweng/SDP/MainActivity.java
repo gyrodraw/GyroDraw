d7e98aa2d77f28e478dffe4696a51a1f42330f09
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-10-02 11:54:01 +0200

    Update with Google Sign In Button

--- app/src/main/java/ch/epfl/sweng/SDP/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/SDP/MainActivity.java
@@ -4,21 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.login_button).setOnClickListener(this);
     }
 
-    /**
-     * Starts the {@link LoginActivity}.
-     * @param view the view corresponding to the clicked button
-     */
-    public void startLogin(View view) {
+    private void startLogin() {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startLogin();
     }
 }

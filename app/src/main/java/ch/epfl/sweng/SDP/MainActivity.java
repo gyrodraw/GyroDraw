58ad6b1130f7a5cecb32f4f949c2430cded67c97
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-10-02 19:56:17 +0200

    Implement new sign-in behavior
    If the user has already signed in, he will be automatically redirected to the
    HomeActivity without having to press the login button again.

--- app/src/main/java/ch/epfl/sweng/SDP/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/SDP/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
@@ -13,15 +14,21 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.login_button).setOnClickListener(this);
-    }
-
-    private void startLogin() {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
     @Override
     public void onClick(View v) {
         startLogin();
+    }
+
+    private void startLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }

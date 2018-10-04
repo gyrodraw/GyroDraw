e7d0fda0bf39eb7dfdc60a617ffee02f5d0dbfbf
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-10-04 18:55:52 +0200

    Update login behavior
    If the user is new or if he has not created an account yet, he is 
    redirected to the account creation page.

--- app/src/main/java/ch/epfl/sweng/SDP/LoginActivity.java
+++ app/src/main/java/ch/epfl/sweng/SDP/LoginActivity.java
@@ -1,5 +1,7 @@
 package ch.epfl.sweng.SDP;
 
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -47,10 +49,21 @@ public class LoginActivity extends AppCompatActivity {
 
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
+
                 // FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser(); for Till
-                Intent intent = new Intent(this, HomeActivity.class);
-                startActivity(intent);
-                finish();
+
+                if (response.isNewUser() || !getDefaultSharedPreferences(getApplicationContext())
+                        .getBoolean("hasAccount", false)) {
+                    // New user or a user who signed in but not created an account
+                    Intent intent = new Intent(this, AccountCreationActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // User has an account
+                    Intent intent = new Intent(this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
             } else {
                 // Sign in failed
                 TextView errorMessage = findViewById(R.id.error_message);

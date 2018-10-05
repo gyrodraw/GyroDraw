05f1e16f8a84bda0793bbfbad42e45257f1d1259
Tillmann Pfeiffer <tillmann.pfeiffer@epfl.ch>
2018-10-05 11:48:37 +0200

    before merge

--- app/src/main/java/ch/epfl/sweng/SDP/AccountCreationActivity.java
+++ app/src/main/java/ch/epfl/sweng/SDP/AccountCreationActivity.java
@@ -8,7 +8,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class AccountCreationActivity extends AppCompatActivity {
     private FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
@@ -42,11 +45,13 @@ public class AccountCreationActivity extends AppCompatActivity {
         t1.setText("clicked");
         t2.setText(Constants.databaseRef.toString());
         String username = usernameInput.getEditText().getText().toString();
-        Constants.databaseRef.child("users").orderByChild("username").equalTo(username).once("value", snapshot => {
-        if(snapshot.exists())  {
+        Constants.databaseRef.child("users").orderByChild("username").equalTo(username).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
 
-        }
-            else {
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });

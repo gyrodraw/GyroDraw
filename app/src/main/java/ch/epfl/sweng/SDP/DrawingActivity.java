95def4b4c15f1764ac015be81023fc8724e5c3f1
Rafael Bischof <rafael.bischof@outlook.com>
2018-10-07 22:39:53 +0100

    drawing with accelerometer working

--- app/src/main/java/ch/epfl/sweng/SDP/DrawingActivity.java
+++ app/src/main/java/ch/epfl/sweng/SDP/DrawingActivity.java
@@ -1,103 +1,124 @@
 package ch.epfl.sweng.SDP;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
+import android.view.View;
 
-public class DrawingActivity extends AppCompatActivity implements SensorEventListener {
+
+public class DrawingActivity extends AppCompatActivity implements SensorEventListener{
+    private CanvasView canvas;
 
     private static final String TAG = "DrawingActivity";
-    private PaintView paintView;
+    private int circleRadius;
+    private float circleX;
+    private float circleY;
+    private int speed;
+    private Path path;
+    private Point size;
+    private Handler handler;
     private SensorManager sensorManager;
-    private float[] accelerometerReading = new float[3];
-    private float[] magnetometerReading = new float[3];
-
-    private float[] rotationMatrix = new float[9];
-    private float[] orientationAngles = new float[3];
+    private Sensor accelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawing);
-        paintView = findViewById(R.id.paintView);
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        paintView.init(metrics);
 
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-    }
+        circleRadius = 8;
+        speed = 5;
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    @Override
-    protected void onResume() {
-        super.onResume();
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
 
-        // Get updates from the accelerometer and magnetometer at a constant rate.
-        // To make batch operations more efficient and reduce power consumption,
-        // provide support for delaying updates to the application.
+        final Display display = getWindowManager().getDefaultDisplay();
+        size = new Point();
+        display.getSize(size);
 
-        // In this example, the sensor reporting delay is small enough such that
-        // the application receives an update before the system checks the sensor
-        // readings again.
+        circleX = size.x / 2 - circleRadius;
+        circleY = size.y / 2 - circleRadius;
 
-        // Register accelerometer sensor listener
-        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if (accelerometer != null) {
-            sensorManager.registerListener(this, accelerometer,
-                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
-        }
+        canvas = new CanvasView(DrawingActivity.this);
+        setContentView(canvas);
+        path = new Path();
+        path.moveTo(circleX, circleY);
 
-        // Register magnetic
-        Sensor magneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        if (magneticField != null) {
-            sensorManager.registerListener(this, magneticField,
-                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
-        }
+        handler = new Handler(){
+           @Override
+           public void handleMessage(Message message){
+               canvas.invalidate();
+           }
+        };
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
-        switch(event.sensor.getType()) {
-            case Sensor.TYPE_ACCELEROMETER:
-                System.arraycopy(event.values, 0, accelerometerReading,
-                        0, accelerometerReading.length);
-                break;
-            case Sensor.TYPE_MAGNETIC_FIELD:
-                System.arraycopy(event.values, 0, magnetometerReading,
-                        0, magnetometerReading.length);
-                break;
-            default:
-                Log.d(TAG, "Should not come here");
-        }
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
 
-        updateOrientationAngles();
+        if(mySensor.getType() == Sensor.TYPE_ACCELEROMETER){
+
+            circleX -= sensorEvent.values[0] * speed;
+            circleY += sensorEvent.values[1] * speed;
+            if(circleX < 0) circleX = 0;
+            else if(circleX > size.x){
+                circleX = size.x;
+            }
+            if(circleY < 0) circleY = 0;
+            else if (circleY > size.y){
+                circleY = size.y;
+            } else {
+                Log.d(TAG, "Strange sensor event registered...");
+            }
+            handler.sendEmptyMessage(0);
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Does nothing for the moment
+        //does nothing
     }
 
-    /**
-     * Updates the rotation matrix according to the sensors to
-     * finally compute the orientation angles.
-     */
-    public void updateOrientationAngles() {
-        // Update rotation matrix, which is needed to update orientation angles.
-        SensorManager.getRotationMatrix(rotationMatrix, null,
-                accelerometerReading, magnetometerReading);
+    private class CanvasView extends View {
 
-        // "rotationMatrix" now has up-to-date information.
+        Paint paint;
 
-        SensorManager.getOrientation(rotationMatrix, orientationAngles);
+        public CanvasView(Context context){
+            super(context);
+            setFocusable(true);
+            paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setDither(true);
+            paint.setColor(Color.RED);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeJoin(Paint.Join.ROUND);
+            paint.setStrokeWidth(10);
+            paint.setStrokeCap(Paint.Cap.ROUND);
+            paint.setXfermode(null);
+            paint.setAlpha(0xff);
+        }
 
-        // "orientationAngles" now has up-to-date information.
+        public void onDraw(Canvas canvas){
+            canvas.save();
+            path.lineTo(circleX, circleY);
+            canvas.drawCircle(circleX, circleY, circleRadius, paint);
+            canvas.drawPath(path, paint);
+            canvas.restore();
+            path.moveTo(circleX, circleY);
+        }
     }
+
 }

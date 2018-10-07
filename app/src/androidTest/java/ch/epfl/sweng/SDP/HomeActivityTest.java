8dca3634b28b265cfb4caaaa7f0e4cee755404f2
LudoHoff <ludo.hoffstetter@epfl.ch>
2018-10-07 17:23:40 +0200

    Fixed lexicographical order of imports

--- app/src/androidTest/java/ch/epfl/sweng/SDP/HomeActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/HomeActivityTest.java
@@ -6,10 +6,9 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static ch.epfl.sweng.SDP.HomeActivity.disableBackgroundAnimation;
-
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import static ch.epfl.sweng.SDP.HomeActivity.disableBackgroundAnimation;
 
 import org.junit.Rule;
 import org.junit.Test;

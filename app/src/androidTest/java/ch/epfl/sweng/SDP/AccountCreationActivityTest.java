4be7b6a1a120ea96d9f87089c97490dbbc9d5b7d
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-10-09 19:50:07 +0200

    Modify a test

--- app/src/androidTest/java/ch/epfl/sweng/SDP/AccountCreationActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/AccountCreationActivityTest.java
@@ -1,18 +1,17 @@
 package ch.epfl.sweng.SDP;
 
 import static android.preference.PreferenceManager.getDefaultSharedPreferences;
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import android.app.Activity;
-import android.app.Instrumentation;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import org.junit.Rule;
@@ -26,15 +25,12 @@ public class AccountCreationActivityTest {
     public final ActivityTestRule<AccountCreationActivity> mActivityRule =
             new ActivityTestRule<>(AccountCreationActivity.class);
 
-    // Add a monitor for the login activity
-    private final Instrumentation.ActivityMonitor monitor = getInstrumentation()
-            .addMonitor(HomeActivity.class.getName(), null, false);
-
     @Test
-    public void testCanOpenLoginActivity() {
-        Activity homeActivity = getInstrumentation()
-                .waitForMonitorWithTimeout(monitor, 5000);
-        assertNotNull(homeActivity);
+    public void testCanOpenHomeActivity() {
+        Intents.init();
+        HomeActivity.disableBackgroundAnimation();
+        onView(withId(R.id.create_account_button)).perform(click());
+        intended(hasComponent(HomeActivity.class.getName()));
     }
 
     @Test

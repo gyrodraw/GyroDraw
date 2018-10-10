57c4e9d6582e987139961ae9af8eb464e69ccd3b
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-10-10 21:23:47 +0200

    Create new VotingPageActivity

---
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/VotingPageActivityTest.java
@@ -0,0 +1,35 @@
+package ch.epfl.sweng.SDP;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class VotingPageActivityTest {
+
+    @Rule
+    public final ActivityTestRule<VotingPageActivity> mActivityRule =
+            new ActivityTestRule<>(VotingPageActivity.class);
+
+    @Test
+    public void testDrawingIsVisible() {
+        onView(withId(R.id.drawing)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testPlayerNameIsVisible() {
+        onView(withId(R.id.playerNameView)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testRatingBarIsVisible() {
+        onView(withId(R.id.ratingBar)).check(matches(isDisplayed()));
+    }
+}

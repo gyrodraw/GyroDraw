0f9ed1cf307c5e176827b986dbe92b033dfec783
Fredrik Bixo <fredrik.bixo@appricos.com>
2018-10-12 00:08:44 +0200

    Fix coding style

--- functions/index.js
+++ functions/index.js
@@ -12,51 +12,36 @@ exports.joinGame = functions.https.onRequest((req, res) => {
   // Grab the text parameter.
   const original = req.query.text;
   // Grab the id of the player
-
   // Check if room is already available and join available room
-
-
   return admin.database().ref('rooms').once("value").then(x => {
     var rooms = x.val();
     console.log(rooms);
     for (var room in rooms) {
-
-
       try{
         if(typeof rooms[room].users !== 'undefined') {
-
-
           console.log(Object.keys(rooms[room].users).length);
-
           if (Object.keys(rooms[room].users).length < 5 && rooms[room].playing === false) {
             if (Object.keys(rooms[room].users).length ===  4) {
               // set playing to true
               admin.database().ref('rooms').child(room).child('playing').set(true)
-
             }
-
             return  admin.database().ref('rooms').child(room).child('users').push().set({
               name: "fredrik"
             }).then(() => {
               return res.status(200).end();
             });
           }
-
         }
       }catch(e){
         console.log('rooms[room].users is undefined');
       }
-
     }
-
-
     return  admin.database().ref('rooms').push().set({
       playing: false,
       users: { "42343243" : { name : "fredrik"} }
     }).then( () => {
       return res.status(200).end();
     });
-
   });
 
 

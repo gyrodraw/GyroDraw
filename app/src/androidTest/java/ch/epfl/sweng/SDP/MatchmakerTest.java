a0d2a50de036c84ad23a1d9fdcde88f55c21d5fe
Fredrik Bixo <bixo@kth.se>
2018-10-13 16:54:50 +0200

    Add unit tests

---
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/MatchmakerTest.java
@@ -0,0 +1,21 @@
+package ch.epfl.sweng.SDP;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MatchmakerTest {
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void joinRoom() {
+    }
+
+    @Test
+    public void leaveRoom() {
+    }
+
+}

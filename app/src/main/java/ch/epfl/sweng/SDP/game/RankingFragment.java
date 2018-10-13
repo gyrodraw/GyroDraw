a11e90723e4e414e44aebe5e8cdf06d991f7a833
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-10-13 19:40:22 +0200

    Add new RankingFragment for the final ranking at the end of the  game

---
+++ app/src/main/java/ch/epfl/sweng/SDP/game/RankingFragment.java
@@ -0,0 +1,63 @@
+package ch.epfl.sweng.SDP.game;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import ch.epfl.sweng.SDP.R;
+import java.util.Locale;
+
+public class RankingFragment extends ListFragment {
+
+    public RankingFragment() {
+    }
+
+    private String[] ranking;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+
+        ranking = getArguments().getStringArray("Ranking");
+        return inflater.inflate(R.layout.ranking_list_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter<String> adapter = new RankingAdapter(getActivity(), ranking);
+        setListAdapter(adapter);
+    }
+
+    private class RankingAdapter extends ArrayAdapter<String> {
+
+        private RankingAdapter(Context context, String[] players) {
+            super(context, 0, players);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext())
+                        .inflate(R.layout.ranking_item, parent, false);
+            }
+
+            TextView pos = convertView.findViewById(R.id.position);
+            TextView name = convertView.findViewById(R.id.playerName);
+
+            pos.setText(String.format(Locale.getDefault(), "%d. ", position + 1));
+            name.setText(ranking[position]);
+
+            // Return the completed view to render on screen
+            return convertView;
+        }
+    }
+}

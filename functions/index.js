c9c55ee58b392d96af244d145a2523709825966b
Claudio5 <claudio.loureiro1@gmail.com>
2018-10-13 16:15:33 +0200

    Modify the firebase functions to reflect the mockRooms data + add joinGame function from matchmaking

--- functions/index.js
+++ functions/index.js
@@ -3,30 +3,43 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
+
+const maxPlayers = 5;
+
 admin.initializeApp();
 
-exports.connectedUsers = functions.database.ref('/timer/connectedUsers').onWrite((event) => {
-    console.log("Function connectedUsers");
-    return admin.database().ref('/timer/connectedUsers').once("value")
+exports.connectedUsers = functions.database.ref('/mockRooms/ABCDE/connectedUsers').onWrite((event) => {
+    return admin.database().ref('/mockRooms/ABCDE/connectedUsers').once("value")
     .then(snapshot => {
-      if(snapshot.val() === 5) {
-        admin.database().ref('/timer').update({'startTimer': 1});
+      let ready = true;
+      snapshot.forEach( (child) => {
+        if(child.val() !== 1) {
+          ready = false;
+        }
+      });
+
+      if(ready) {
+        admin.database().ref('/mockRooms/ABCDE/timer/startTimer').set(1);
+        console.log("Ready");
       } else {
-        admin.database().ref('/timer').update({'startTimer': 0});
+        admin.database().ref('/mockRooms/ABCDE/timer/startTimer').set(0);
+        console.log("Not ready");
       }
+
       return;
     });
 });
 
-exports.startTimer = functions.database.ref('/timer/startTimer').onWrite((event) => {
-  return admin.database().ref('/timer/startTimer').once("value")
+exports.startTimer = functions.database.ref('/mockRooms/ABCDE/timer/startTimer').onWrite((event) => {
+  return admin.database().ref('/mockRooms/ABCDE/timer/startTimer').once("value")
   .then(snapshot => {
     if(snapshot.val() === 1) {
       console.log("Timer started");
 
+      // Wait in seconds
       return functionTimer(20,
             elapsedTime => {
-                admin.database().ref('timer/observableTime').set(elapsedTime);
+                admin.database().ref('/mockRooms/ABCDE/timer/observableTime').set(elapsedTime);
             })
             .then(totalTime => {
                 return console.log('Timer of ' + totalTime + ' has finished.');
@@ -60,3 +73,40 @@ function functionTimer (seconds, call) {
         }
     });
 }
+
+exports.joinGame = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Grab the id of the player
+  // Check if room is already available and join available room
+  return admin.database().ref('rooms').once("value").then(x => {
+    var rooms = x.val();
+    console.log(rooms);
+    for (var room in rooms) {
+      try{
+        if(typeof rooms[room].users !== 'undefined') {
+          console.log(Object.keys(rooms[room].users).length);
+          if (Object.keys(rooms[room].users).length < 5 && rooms[room].playing === false) {
+            if (Object.keys(rooms[room].users).length ===  4) {
+              // set playing to true
+              admin.database().ref('rooms').child(room).child('playing').set(true)
+            }
+            return  admin.database().ref('rooms').child(room).child('users').push().set({
+              name: "fredrik"
+            }).then(() => {
+              return res.status(200).end();
+            });
+          }
+        }
+      }catch(e){
+        console.log('rooms[room].users is undefined');
+      }
+    }
+    return  admin.database().ref('rooms').push().set({
+      playing: false,
+      users: { "42343243" : { name : "fredrik"} }
+    }).then( () => {
+      return res.status(200).end();
+    });
+  });
+});

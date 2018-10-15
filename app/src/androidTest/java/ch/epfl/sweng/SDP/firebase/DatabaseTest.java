db9b9279da2f580db9932a0cd148ef517e330594
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-10-15 18:03:54 +0200

    Update tests

--- app/src/androidTest/java/ch/epfl/sweng/SDP/firebase/DatabaseTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/firebase/DatabaseTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.is;
 
 import android.support.test.InstrumentationRegistry;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
 import org.junit.Test;
 
 public class DatabaseTest {
@@ -17,9 +18,28 @@ public class DatabaseTest {
     }
 
     @Test
+    public void getReferenceWithSingleKeyReturnsValidReference() {
+        FirebaseApp.initializeApp(InstrumentationRegistry.getContext());
+        Database database = Database.getInstance();
+        DatabaseReference ref = database.getReference("test");
+        assertThat(ref.getKey(), is("test"));
+    }
+
+    @Test
+    public void getReferenceWithMultipleKeysReturnsValidReference() {
+        FirebaseApp.initializeApp(InstrumentationRegistry.getContext());
+        Database database = Database.getInstance();
+        DatabaseReference ref = database.getReference("test.tests");
+        assertThat(ref.getKey(), is("tests"));
+        assertThat(ref.getParent().getKey(), is("test"));
+    }
+
+    @Test
     public void getInstanceShouldAlwaysReturnTheSameInstance() {
         FirebaseApp.initializeApp(InstrumentationRegistry.getContext());
         Database database = Database.getInstance();
         assertThat(Database.getInstance(), is(database));
     }
+
+
 }

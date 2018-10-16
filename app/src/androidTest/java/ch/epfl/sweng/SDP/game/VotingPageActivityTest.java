6b844561b3e7d8d6fe42544e4c890329cc5af078
Claudio5 <claudio.loureiro1@gmail.com>
2018-10-16 16:18:24 +0200

    Add more tests for voting page

--- app/src/androidTest/java/ch/epfl/sweng/SDP/game/VotingPageActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/game/VotingPageActivityTest.java
@@ -1,15 +1,21 @@
 package ch.epfl.sweng.SDP.game;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.is;
 
+import android.graphics.drawable.Drawable;
 import android.os.SystemClock;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import ch.epfl.sweng.SDP.R;
 import org.junit.Rule;
@@ -33,5 +39,35 @@ public class VotingPageActivityTest {
         ((RatingBar) mActivityRule.getActivity().findViewById(R.id.ratingBar)).setRating(3);
         SystemClock.sleep(1000);
         assertThat(mActivityRule.getActivity().getRatings()[0], is(3));
+    }
+
+    @Test
+    public void progressBarTest() {
+        Drawable notAnimatedDrawable = ContextCompat.getDrawable(mActivityRule.getActivity(), R.drawable.draw_button);
+        ((ProgressBar) mActivityRule.getActivity().findViewById(R.id.progressBar)).setIndeterminateDrawable(notAnimatedDrawable);
+
+        onView(withId(R.id.progressBar)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void changeImageButtonIsClickable() {
+        onView(withId(R.id.buttonChangeImage)).check(matches(isClickable()));
+        onView(withId(R.id.rankingButton)).perform(click());
+    }
+
+    @Test
+    public void changeImageButtonIsDisplayed() {
+        onView(withId(R.id.buttonChangeImage)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void rankingButtonIsClickable() {
+        onView(withId(R.id.rankingButton)).check(matches(isClickable()));
+        onView(withId(R.id.rankingButton)).perform(click());
+    }
+
+    @Test
+    public void rankingButtonIsDisplayed() {
+        onView(withId(R.id.rankingButton)).check(matches(isDisplayed()));
     }
 }

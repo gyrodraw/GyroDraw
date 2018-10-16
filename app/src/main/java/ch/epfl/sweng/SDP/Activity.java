8dbc88e404d6aa7e03778d047a3878adabf3ea9c
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-10-16 21:23:18 +0200

    Add setVisibility methods

--- app/src/main/java/ch/epfl/sweng/SDP/Activity.java
+++ app/src/main/java/ch/epfl/sweng/SDP/Activity.java
@@ -2,6 +2,7 @@ package ch.epfl.sweng.SDP;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Class containing useful and widely used methods. It should be inherited by all the other
@@ -17,5 +18,27 @@ public abstract class Activity extends AppCompatActivity {
     public void launchActivity(Class activityClass) {
         Intent intent = new Intent(this, activityClass);
         startActivity(intent);
+    }
+
+    /**
+     * Set the visibility of the views corresponding to the given ids to the given value.
+     * @param visibility the value to set the visibility at
+     * @param ids the ids of the views whose visibility is to be set
+     */
+    public void setVisibility(int visibility, int... ids) {
+        for (int id: ids) {
+            findViewById(id).setVisibility(visibility);
+        }
+    }
+
+    /**
+     * Set the visibility of the given views to the given value.
+     * @param visibility the value to set the visibility at
+     * @param views the views whose visibility is to be set
+     */
+    public void setVisibility(int visibility, View... views) {
+        for (View view: views) {
+            view.setVisibility(visibility);
+        }
     }
 }

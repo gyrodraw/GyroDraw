aed8bc46b3957fa872013c6e1a02cebdb7fadb42
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-10-18 19:45:38 +0200

    Refactor

--- app/src/main/java/ch/epfl/sweng/SDP/game/VotingPageActivity.java
+++ app/src/main/java/ch/epfl/sweng/SDP/game/VotingPageActivity.java
@@ -2,7 +2,6 @@ package ch.epfl.sweng.SDP.game;
 
 import static java.lang.String.format;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -137,7 +136,7 @@ public class VotingPageActivity extends Activity {
         // Get the players' names
         playersNames = new String[]{"Player0", "Player1", "Player2", "Player3",
                 "Player4"}; // hardcoded now, need to be given by the
-                            // server/script or retrieved from database
+        // server/script or retrieved from database
 
         ratings = new int[NUMBER_OF_DRAWINGS];
         ratingBar = findViewById(R.id.ratingBar);
@@ -190,6 +189,7 @@ public class VotingPageActivity extends Activity {
 
     /**
      * Switch the drawing when clicking the button.
+     *
      * @param view View referencing the button
      */
     public void changeImage(View view) {
@@ -281,6 +281,7 @@ public class VotingPageActivity extends Activity {
 
     /**
      * Show the final ranking in a new fragment.
+     *
      * @param view View referencing the button
      */
     public void showFinalRanking(View view) {

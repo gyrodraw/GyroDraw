0ee60de95c7f5a6f70f7e61ea65462b3a127e5b5
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-10-19 11:48:15 +0200

    Adapt the tests to the new Database class modifications

--- app/src/androidTest/java/ch/epfl/sweng/SDP/matchmaking/MatchmakerTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/matchmaking/MatchmakerTest.java
@@ -2,35 +2,27 @@ package ch.epfl.sweng.SDP.matchmaking;
 
 import static org.junit.Assert.assertTrue;
 
-import ch.epfl.sweng.SDP.matchmaking.Matchmaker;
 import org.junit.Test;
 
 public class MatchmakerTest {
 
     @Test
-    public void getInstance() {
-        Matchmaker.getInstance();
-        assertTrue(true);
-    }
-
-    @Test
     public void matchMaker() {
-        Matchmaker matchMaker = new Matchmaker();
-        matchMaker.leaveRoom("2312");
+        Matchmaker.INSTANCE.leaveRoom("2312");
         assertTrue(true);
     }
 
 
     @Test
     public void joinRoom() {
-        Matchmaker.getInstance().joinRoom("2312");
+        Matchmaker.INSTANCE.joinRoom("2312");
         assertTrue(true);
 
     }
 
     @Test
     public void leaveRoom() {
-        Matchmaker.getInstance().leaveRoom("2312");
+        Matchmaker.INSTANCE.leaveRoom("2312");
         assertTrue(true);
     }
 

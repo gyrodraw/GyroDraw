840639d987aa07d453b51370b20ca753a7ca56c3
Tillmann Pfeiffer <tillmann.pfeiffer@epfl.ch>
2018-10-24 00:43:13 +0200

    to check if androidTest works with travis

--- app/src/androidTest/java/ch/epfl/sweng/SDP/shop/ShopActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/shop/ShopActivityTest.java
@@ -1,4 +1,30 @@
 package ch.epfl.sweng.SDP.shop;
 
+import static android.support.test.espresso.Espresso.onView;
+
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ch.epfl.sweng.SDP.R;
+import ch.epfl.sweng.SDP.home.HomeActivity;
+
+@RunWith(AndroidJUnit4.class)
 public class ShopActivityTest {
+
+    @Rule
+    public final ActivityTestRule<ShopActivity> activityTestRule =
+            new ActivityTestRule<>(ShopActivity.class);
+
+    @Test
+    public void returnIsClickable() {
+        onView(ViewMatchers.withId(R.id.returnFromShop)).check(matches(isClickable()));
+    }
 }

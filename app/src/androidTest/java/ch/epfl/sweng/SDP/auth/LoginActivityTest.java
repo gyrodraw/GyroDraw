a9399bfcc2043cb703ea6a8e154a92d7ff32688a
Rafael Bischof <rafael.bischof@outlook.com>
2018-10-25 12:18:57 +0200

    see if test that fails locally (login problems) passes on travis

--- app/src/androidTest/java/ch/epfl/sweng/SDP/auth/LoginActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/auth/LoginActivityTest.java
@@ -2,7 +2,9 @@ package ch.epfl.sweng.SDP.auth;
 
 import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
+import android.widget.TextView;
 
+import com.firebase.ui.auth.FirebaseUiException;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.util.ExtraConstants;
 
@@ -13,8 +15,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mockito;
 
+import ch.epfl.sweng.SDP.R;
+
 import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnit4.class)
 public class LoginActivityTest {
@@ -24,7 +29,8 @@ public class LoginActivityTest {
             new ActivityTestRule<>(LoginActivity.class);
 
     Intent mockIntent;
-    IdpResponse mockIdpResponse = Mockito.mock(IdpResponse.class);
+    IdpResponse mockIdpResponse;
+    FirebaseUiException mockError;
     LoginActivity loginActivity;
 
     /**
@@ -34,13 +40,14 @@ public class LoginActivityTest {
     public void init(){
         mockIntent = Mockito.mock(Intent.class);
         mockIdpResponse = Mockito.mock(IdpResponse.class);
+        mockError = Mockito.mock(FirebaseUiException.class);
         loginActivity = activityRule.getActivity();
+        Mockito.when(mockIntent.getParcelableExtra(ExtraConstants.IDP_RESPONSE))
+                .thenReturn(mockIdpResponse);
     }
 
     @Test
     public void testSuccessfulLogin(){
-        Mockito.when(mockIntent.getParcelableExtra(ExtraConstants.IDP_RESPONSE))
-                .thenReturn(mockIdpResponse);
         Mockito.when(mockIdpResponse.isNewUser()).thenReturn(true);
         loginActivity.onActivityResult(42, -1, mockIntent);
         assertTrue(loginActivity.isFinishing());
@@ -50,30 +57,24 @@ public class LoginActivityTest {
     public void testExistingUser(){
         getDefaultSharedPreferences(activityRule.getActivity()).edit()
                 .putBoolean("hasAccount", true).apply();
-        Mockito.when(mockIntent.getParcelableExtra(ExtraConstants.IDP_RESPONSE))
-                .thenReturn(mockIdpResponse);
         Mockito.when(mockIdpResponse.isNewUser()).thenReturn(false);
         loginActivity.onActivityResult(42, -1, mockIntent);
         assertTrue(loginActivity.isFinishing());
     }
 
-    //@Test
-    //public void testNoInternetConnection(){
-    //    Mockito.when(mockIntent.getParcelableExtra(ExtraConstants.IDP_RESPONSE))
-    //          .thenReturn(mockIdpResponse);
-    //    Mockito.when(mockIdpResponse.getError()).thenReturn(mockError);
-    //    Mockito.when(mockError.getErrorCode()).thenReturn(1);
-    //    loginActivity.onActivityResult(42, 0, mockIntent);
-    //    assertEquals(((TextView)loginActivity.findViewById(R.id.error_message))
-    //          .getText(), "No Internet connection");
-    //}
+    @Test
+    public void testNoInternetConnection(){
+        Mockito.when(mockIdpResponse.getError()).thenReturn(mockError);
+        Mockito.when(mockError.getErrorCode()).thenReturn(1);
+        loginActivity.onActivityResult(42, 0, mockIntent);
+        assertEquals(((TextView)loginActivity.findViewById(R.id.error_message))
+              .getText(), "No Internet connection");
+    }
 
     @Test
     public void testFailedLoginNullResponse(){
         mockIntent = Mockito.mock(Intent.class);
         mockIdpResponse = null;
-        Mockito.when(mockIntent.getParcelableExtra(ExtraConstants.IDP_RESPONSE))
-                .thenReturn(mockIdpResponse);
         loginActivity.onActivityResult(42, 0, mockIntent);
         assertTrue(loginActivity.isFinishing());
     }

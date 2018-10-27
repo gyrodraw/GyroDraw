dc581d03bcd6eab2501f195fa0165a4246a4b88d
Claudio5 <claudio.loureiro1@gmail.com>
2018-10-27 23:13:05 +0200

    Create an activity that will represent a loading screen

---
+++ app/src/main/java/ch/epfl/sweng/SDP/game/LoadingScreen.java
@@ -0,0 +1,140 @@
+package ch.epfl.sweng.SDP.game;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.functions.FirebaseFunctionsException;
+
+import java.util.Vector;
+
+import ch.epfl.sweng.SDP.BooleanVariableListener;
+import ch.epfl.sweng.SDP.R;
+import ch.epfl.sweng.SDP.firebase.Database;
+import ch.epfl.sweng.SDP.matchmaking.Matchmaker;
+
+public class LoadingScreen extends AppCompatActivity {
+
+    private String roomID = null;
+
+    private BooleanVariableListener isRoomReady = new BooleanVariableListener();
+    private BooleanVariableListener areWordsReady = new BooleanVariableListener();
+    private static boolean enableWaitingAnimation = true;
+
+    private static final String WORD_CHILDREN_DB_ID = "words";
+    private static final String TOP_ROOM_NODE_ID = "realRooms";
+
+    private boolean isWord1Ready = false;
+    private boolean isWord2Ready = false;
+
+    private DatabaseReference wordsVotesRef;
+
+    private String word1 = "";
+    private String word2 = "";
+
+    private Database database;
+
+    private BooleanVariableListener.ChangeListener listenerRoomReady =
+            new BooleanVariableListener.ChangeListener() {
+                @Override
+                public void onChange() {
+                    if(areWordsReady.getBoo() && isRoomReady.getBoo()) {
+                        // Start new activity
+                        wordsVotesRef.removeEventListener(listenerWords);
+                        Intent intent = new Intent(getApplicationContext(), WaitingPageActivity.class);
+                        intent.putExtra("word1", word1);
+                        intent.putExtra("word2", word2);
+                        intent.putExtra("roomID", roomID);
+                        startActivity(intent);
+                    }
+                }
+            };
+
+    private final ValueEventListener listenerWords = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            Vector<String> words = new Vector<>();
+
+            for(DataSnapshot snap : dataSnapshot.getChildren()) {
+                words.add(snap.getKey());
+            }
+
+            try {
+                word1 = words.get(0);
+                word2 = words.get(1);
+            } catch (Exception e) {
+                // Throws exception
+            }
+
+            if (word1 != null) {
+                isWord1Ready = true;
+            }
+
+            if (word2 != null) {
+                isWord2Ready = true;
+            }
+
+            if(isWord1Ready && isWord2Ready) {
+                areWordsReady.setBoo(true);
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+            throw databaseError.toException();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+
+        database = Database.INSTANCE;
+        lookingForRoom();
+
+        overridePendingTransition(0, 0);
+
+        isRoomReady.setListener(listenerRoomReady);
+        areWordsReady.setListener(listenerRoomReady);
+
+        Glide.with(this).load(R.drawable.waiting_animation_dots)
+                .into((ImageView) findViewById(R.id.waitingAnimationDots));
+        Glide.with(this).load(R.drawable.background_animation)
+                .into((ImageView) findViewById(R.id.waitingBackgroundAnimation));
+
+    }
+
+    private void lookingForRoom() {
+        Matchmaker.INSTANCE.joinRoom().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if (!task.isSuccessful()) {
+                    Exception exception = task.getException();
+                    if (exception instanceof FirebaseFunctionsException) {
+                        FirebaseFunctionsException ffe = (FirebaseFunctionsException) exception;
+                    }
+                } else {
+                    roomID = task.getResult();
+                    wordsVotesRef = database.getReference(
+                            TOP_ROOM_NODE_ID + "." + roomID + "." + WORD_CHILDREN_DB_ID);
+                    wordsVotesRef.addValueEventListener(listenerWords);
+                    isRoomReady.setBoo(true);
+                }
+            }
+        });
+    }
+}

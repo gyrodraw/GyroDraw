e60135bb0a09285546d944f68168dec8f7a6944b
Claudio5 <claudio.loureiro1@gmail.com>
2018-10-28 22:07:21 +0100

    Modify tests in loading screen

--- app/src/androidTest/java/ch/epfl/sweng/SDP/game/LoadingScreenActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/game/LoadingScreenActivityTest.java
@@ -1,25 +1,33 @@
 package ch.epfl.sweng.SDP.game;
 
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 
+import org.hamcrest.Matcher;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
+import java.util.concurrent.TimeUnit;
+
 import ch.epfl.sweng.SDP.R;
+import ch.epfl.sweng.SDP.game.drawing.DrawingActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static ch.epfl.sweng.SDP.game.LoadingScreenActivity.disableLoadingAnimations;
-import static ch.epfl.sweng.SDP.game.LoadingScreenActivity.setOnTest;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
 
 @RunWith(AndroidJUnit4.class)
 public class LoadingScreenActivityTest {
@@ -31,30 +39,45 @@ public class LoadingScreenActivityTest {
             @Override
             protected void beforeActivityLaunched() {
                 disableLoadingAnimations();
-                setOnTest();
             }
     };
 
     @Test
     public void drawableMatchTest() {
+        Intents.init();
         onView(withId(R.id.waitingBackgroundAnimation)).perform(click());
         onView(withId(R.id.waitingAnimationDots)).perform(click());
         onView(withId(R.id.waitingBackgroundImage)).perform(click());
-    }
 
-    @Test
-    public void areViewDisplayed() {
         onView(withId(R.id.waitingBackgroundAnimation)).check(matches(isDisplayed()));
         onView(withId(R.id.waitingAnimationDots)).check(matches(isDisplayed()));
         onView(withId(R.id.waitingBackgroundImage)).check(matches(isDisplayed()));
+
+        onView(isRoot()).perform(waitFor(TimeUnit.SECONDS.toMillis(10)));
+        intended(hasComponent(WaitingPageActivity.class.getName()));
+        Intents.release();
     }
 
-    @Test
-    public void testLookingForRoom() {
-        LoadingScreenActivity mock = Mockito.spy(mActivityRule.getActivity());
-        doNothing().when(mock).lookingForRoom();
+    /**
+     * Perform action of waiting for a specific time.
+     */
+    @Ignore
+    public static ViewAction waitFor(final long millis) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
 
-        mock.lookingForRoom();
-        verify(mock, atLeastOnce()).lookingForRoom();
+            @Override
+            public String getDescription() {
+                return "Wait for " + millis + " milliseconds.";
+            }
+
+            @Override
+            public void perform(UiController uiController, final View view) {
+                uiController.loopMainThreadForAtLeast(millis);
+            }
+        };
     }
 }

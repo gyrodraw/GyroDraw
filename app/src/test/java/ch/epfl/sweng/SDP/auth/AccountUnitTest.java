566c4721123449012e060252ab1a72102160c045
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-10-29 17:33:00 +0100

    Fix tests
    Now they work with the current Account implementation

--- app/src/test/java/ch/epfl/sweng/SDP/auth/AccountUnitTest.java
+++ app/src/test/java/ch/epfl/sweng/SDP/auth/AccountUnitTest.java
@@ -2,120 +2,137 @@ package ch.epfl.sweng.SDP.auth;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
+import android.content.Context;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.mockito.Mockito;
 
+@RunWith(JUnit4.class)
 public class AccountUnitTest {
 
     private ConstantsWrapper mockConstantsWrapper;
-    private Account account = Account.getInstance();
+    private Context mockContext;
+    private Account mockAccount;
 
     @Before
     public void init() {
         mockConstantsWrapper = Mockito.mock(ConstantsWrapper.class);
+        mockContext = Mockito.mock(Context.class);
+        mockAccount = Mockito.mock(Account.class);
+        DatabaseReference mockReference = Mockito.mock(DatabaseReference.class);
+        Query mockQuery = Mockito.mock(Query.class);
 
         when(mockConstantsWrapper.getUsersRef()).thenReturn(mockReference);
         when(mockConstantsWrapper.getFirebaseUserId()).thenReturn("123456789");
+
         when(mockReference.child(isA(String.class))).thenReturn(mockReference);
         when(mockReference.orderByChild(isA(String.class))).thenReturn(mockQuery);
+
         when(mockQuery.equalTo(isA(String.class))).thenReturn(mockQuery);
+
         doNothing().when(mockReference).setValue(isA(Integer.class), isA(DatabaseReference.CompletionListener.class));
         doNothing().when(mockReference).setValue(isA(Boolean.class), isA(DatabaseReference.CompletionListener.class));
         doNothing().when(mockReference).removeValue(isA(DatabaseReference.CompletionListener.class));
+
         doNothing().when(mockQuery).addListenerForSingleValueEvent(isA(ValueEventListener.class));
 
-        account.setAccount(mockConstantsWrapper, "123456789");
-        account.setUserId("123456789");
+        doNothing().when(mockAccount).registerAccount();
+        doNothing().when(mockAccount).updateUsername(isA(String.class));
+
+        Account.createAccount(mockContext, mockConstantsWrapper, "123456789");
+        Account.getInstance(mockContext).setUserId("123456789");
+        Account.enableTesting();
     }
 
     @Test
     public void testAccountValuesCorrect() {
-        assertEquals(account.getTrophies(), 0);
-        assertEquals(account.getStars(), 0);
-        assertEquals(account.getUsername(), "123456789");
-        assertEquals(mockConstantsWrapper.getFirebaseUserId(), "123456789");
+        assertThat(Account.getInstance(mockContext).getTrophies(), is(0));
+        assertThat(Account.getInstance(mockContext).getStars(), is(0));
+        assertThat(Account.getInstance(mockContext).getUsername(), is("123456789"));
+        assertThat(mockConstantsWrapper.getFirebaseUserId(), is("123456789"));
     }
 
     @Test
     public void testGetStars() {
-        assertThat(account.getStars(), is(0));
+        assertThat(Account.getInstance(mockContext).getStars(), is(0));
     }
 
     @Test
     public void testGetUsername() {
-        assertThat(account.getUsername(), is("123456789"));
+        assertThat(Account.getInstance(mockContext).getUsername(), is("123456789"));
     }
 
     @Test
     public void testGetTrophies() {
-        assertThat(account.getTrophies(), is(0));
+        assertThat(Account.getInstance(mockContext).getTrophies(), is(0));
     }
 
     @Test
     public void testChangeTrophies() {
-        account.changeTrophies(20);
+        Account.getInstance(mockContext).changeTrophies(20);
         //assertEquals(account.getTrophies(), 20);
     }
 
     @Test
     public void testAddStars() {
-        account.addStars(20);
+        Account.getInstance(mockContext).changeStars(20);
         //assertEquals(account.getStars(), 20);
     }
 
     @Test
     public void testAddFriend() {
-        account.addFriend("EPFLien");
+        Account.getInstance(mockContext).addFriend("EPFLien");
     }
 
     @Test
     public void testRemoveFriend() {
-        account.removeFriend("EPFLien");
+        Account.getInstance(mockContext).removeFriend("EPFLien");
     }
 
     @Test
     public void testUpdateUsername() {
-        account.updateUsername("987654321");
-        assertEquals(account.getUsername(), "987654321");
+        mockAccount.updateUsername("987654321");
+        Account.getInstance(mockContext).setUsername("987654321");
+        assertThat(Account.getInstance(mockContext).getUsername(), is("987654321"));
     }
 
     @Test
     public void testRegisterAccount() {
-        account.registerAccount();
+        mockAccount.registerAccount();
     }
 
     @Test
     public void testCheckIfUsernameTaken() {
-        account.checkIfAccountNameIsFree("123456789");
-        assertEquals(account.getUsername(), "123456789");
+        Account.getInstance(mockContext).checkIfAccountNameIsFree("123456789");
+        assertThat(Account.getInstance(mockContext).getUsername(), is("123456789"));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testNullUsername() {
-        account.updateUsername(null);
+        Account.getInstance(mockContext).updateUsername(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testAddNegativeBalanceStars() {
-        account.addStars(-1000);
+        Account.getInstance(mockContext).changeStars(-1000);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testAddNullFriend() {
-        account.addFriend(null);
+        Account.getInstance(mockContext).addFriend(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void removeNullFriend() {
-        account.removeFriend(null);
+        Account.getInstance(mockContext).removeFriend(null);
     }
 }

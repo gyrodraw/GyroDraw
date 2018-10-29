131e7c19734fbe704b57cc84a0fde278bf9e1240
Rafael Bischof <rafael.bischof@outlook.com>
2018-10-29 11:09:55 +0100

    enabled full screen mode in drawingActivity and forced portrait mode in entire application

--- app/src/main/java/ch/epfl/sweng/SDP/game/drawing/DrawingActivity.java
+++ app/src/main/java/ch/epfl/sweng/SDP/game/drawing/DrawingActivity.java
@@ -90,6 +90,15 @@ public class DrawingActivity extends BaseActivity implements SensorEventListener
                 paintView.invalidate();
             }
         };
+
+        // hides UI bar
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
     /**

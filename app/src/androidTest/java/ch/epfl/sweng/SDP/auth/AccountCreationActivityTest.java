ee5896c4a4dbb05c7dcbae1ba2869e56105825f4
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-10-30 08:57:27 +0100

    Remove unused imports

--- app/src/androidTest/java/ch/epfl/sweng/SDP/auth/AccountCreationActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/auth/AccountCreationActivityTest.java
@@ -1,32 +1,21 @@
 package ch.epfl.sweng.SDP.auth;
 
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static junit.framework.TestCase.assertTrue;
 
-import android.app.Activity;
-import android.app.Instrumentation;
-import android.support.test.espresso.intent.Intents;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Assert;
+import ch.epfl.sweng.SDP.R;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import ch.epfl.sweng.SDP.R;
-import ch.epfl.sweng.SDP.home.HomeActivity;
 
 
 @RunWith(AndroidJUnit4.class)

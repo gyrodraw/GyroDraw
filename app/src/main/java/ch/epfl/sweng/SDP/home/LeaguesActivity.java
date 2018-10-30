7d4d54a402eb9a296452a4c9123883e84eac0ae7
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-10-30 19:39:05 +0100

    Add background image+animation and right font

--- app/src/main/java/ch/epfl/sweng/SDP/home/LeaguesActivity.java
+++ app/src/main/java/ch/epfl/sweng/SDP/home/LeaguesActivity.java
@@ -1,8 +1,12 @@
 package ch.epfl.sweng.SDP.home;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import ch.epfl.sweng.SDP.Activity;
 import ch.epfl.sweng.SDP.R;
+import com.bumptech.glide.Glide;
 
 public class LeaguesActivity extends Activity {
 
@@ -10,5 +14,17 @@ public class LeaguesActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leagues);
+
+        Typeface typeOptimus = Typeface.createFromAsset(getAssets(), "fonts/Optimus.otf");
+        setGlobalTypeface(typeOptimus);
+
+        Glide.with(this).load(R.drawable.background_animation)
+                .into((ImageView) findViewById(R.id.leaguesBackgroundAnimation));
+    }
+
+    private void setGlobalTypeface(Typeface typeface) {
+        ((TextView) findViewById(R.id.league1Name)).setTypeface(typeface);
+        ((TextView) findViewById(R.id.league2Name)).setTypeface(typeface);
+        ((TextView) findViewById(R.id.league3Name)).setTypeface(typeface);
     }
 }

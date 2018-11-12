01197cca8f8b0f1e7ce4c37501807a61658747e1
LudoHoff <ludo.hoffstetter@epfl.ch>
2018-11-12 10:20:13 +0100

    Fix broken tests

--- app/src/androidTest/java/ch/epfl/sweng/SDP/auth/AccountCreationActivityAndAccountTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/auth/AccountCreationActivityAndAccountTest.java
@@ -232,12 +232,12 @@ public class AccountCreationActivityAndAccountTest {
 
     @Test
     public void testCreateAccIsClickable() {
-        onView(ViewMatchers.withId(R.id.createAcc)).check(matches(isClickable()));
+        onView(ViewMatchers.withId(R.id.createAccount)).check(matches(isClickable()));
     }
 
     @Test
     public void testCreateAccountWithNullName() {
-        onView(ViewMatchers.withId(R.id.createAcc)).perform(click());
+        onView(ViewMatchers.withId(R.id.createAccount)).perform(click());
         onView(ViewMatchers.withId(R.id.usernameTaken))
                 .check(matches(withText("Username must not be empty.")));
     }
@@ -253,7 +253,7 @@ public class AccountCreationActivityAndAccountTest {
     public void testCreateAccountWithValidInput() {
         onView(withId(R.id.usernameInput))
                 .perform(typeText("Max Muster"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.createAcc)).perform(click());
+        onView(ViewMatchers.withId(R.id.createAccount)).perform(click());
         assertNotEquals(null, Account.getInstance(activityRule.getActivity()));
     }
 

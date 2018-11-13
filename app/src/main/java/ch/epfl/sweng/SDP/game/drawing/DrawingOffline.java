031de411b1d9c6d2dea2a6e31059d6a23474b914
Claudio5 <claudio.loureiro1@gmail.com>
2018-11-13 17:26:57 +0100

    Clean imports

--- app/src/main/java/ch/epfl/sweng/SDP/game/drawing/DrawingOffline.java
+++ app/src/main/java/ch/epfl/sweng/SDP/game/drawing/DrawingOffline.java
@@ -1,24 +1,7 @@
 package ch.epfl.sweng.SDP.game.drawing;
 
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
-import android.support.annotation.VisibleForTesting;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
-
-import ch.epfl.sweng.SDP.R;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
 
 public class DrawingOffline extends GyroDrawingActivity {
 

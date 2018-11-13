7562f34f6223f411e1fc71243754d39c3883a1b7
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-11-13 11:49:19 +0100

    Add new Preconditions class useful for checking methods preconditions

---
+++ app/src/main/java/ch/epfl/sweng/SDP/utils/Preconditions.java
@@ -0,0 +1,32 @@
+package ch.epfl.sweng.SDP.utils;
+
+public final class Preconditions {
+
+    private Preconditions() {
+    }
+
+    /**
+     * Check if the given precondition is true; if not, an {@link IllegalArgumentException} with the
+     * given error message is thrown.
+     *
+     * @param precondition the precondition to assert
+     * @param errorMessage the error message to show
+     */
+    public static void checkPrecondition(boolean precondition, String errorMessage) {
+        if (!precondition) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    /**
+     * Check if the given precondition is true; if not, an {@link IllegalArgumentException} is
+     * thrown.
+     *
+     * @param precondition the precondition to assert
+     */
+    public static void checkPrecondition(boolean precondition) {
+        if (!precondition) {
+            throw new IllegalArgumentException();
+        }
+    }
+}

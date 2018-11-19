9f9dc2ae508bd8270f7c6a37225b7a631d708a53
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-11-19 17:57:29 +0100

    Add new annotation VisibleForTesting to deleteAccount() method

--- app/src/main/java/ch/epfl/sweng/SDP/auth/Account.java
+++ app/src/main/java/ch/epfl/sweng/SDP/auth/Account.java
@@ -8,6 +8,7 @@ import static ch.epfl.sweng.SDP.utils.Preconditions.checkPrecondition;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
 import ch.epfl.sweng.SDP.firebase.Database;
 import ch.epfl.sweng.SDP.firebase.Database.DatabaseReferenceBuilder;
 import ch.epfl.sweng.SDP.home.League;
@@ -138,6 +139,7 @@ public class Account {
         return instance;
     }
 
+    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
     public static void deleteAccount() {
         instance = null;
     }

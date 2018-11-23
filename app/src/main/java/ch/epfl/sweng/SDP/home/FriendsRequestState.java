0a4ebeb7da174e4b44401b2438f450b5e1e91708
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-11-23 15:32:21 +0100

    Add new static factory for creating values from integers

--- app/src/main/java/ch/epfl/sweng/SDP/home/FriendsRequestState.java
+++ app/src/main/java/ch/epfl/sweng/SDP/home/FriendsRequestState.java
@@ -1,5 +1,18 @@
 package ch.epfl.sweng.SDP.home;
 
 public enum FriendsRequestState {
-    SENT, RECEIVED, FRIENDS
+    SENT, RECEIVED, FRIENDS;
+
+    public static FriendsRequestState fromInteger(int i) {
+        switch (i) {
+            case 0:
+                return SENT;
+            case 1:
+                return RECEIVED;
+            case 2:
+                return FRIENDS;
+            default:
+                return null;
+        }
+    }
 }

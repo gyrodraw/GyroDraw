3b2a8cba2b385bf5dcce49425e865f65e2fc4c84
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-11-24 13:22:23 +0100

    Add javadocs

--- app/src/main/java/ch/epfl/sweng/SDP/home/FriendsRequestState.java
+++ app/src/main/java/ch/epfl/sweng/SDP/home/FriendsRequestState.java
@@ -1,10 +1,19 @@
 package ch.epfl.sweng.SDP.home;
 
+/**
+ * Enum modelling a friends request state.
+ */
 public enum FriendsRequestState {
     SENT, RECEIVED, FRIENDS;
 
-    public static FriendsRequestState fromInteger(int i) {
-        switch (i) {
+    /**
+     * Builds a {@link FriendsRequestState} value from the given integer.
+     *
+     * @param integer the integer corresponding to the desired enum value
+     * @return an enum value
+     */
+    public static FriendsRequestState fromInteger(int integer) {
+        switch (integer) {
             case 0:
                 return SENT;
             case 1:

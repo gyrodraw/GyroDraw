535cdd77f4b7c68f22f3904fbf4690f6fdbe7cd8
Claudio5 <claudio.loureiro1@gmail.com>
2018-11-26 11:19:43 +0100

    Modify test

--- app/src/androidTest/java/ch/epfl/sweng/SDP/shop/ShopActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/shop/ShopActivityTest.java
@@ -5,11 +5,14 @@ import android.support.test.rule.ActivityTestRule;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.google.firebase.database.DatabaseReference;
+
 import org.junit.Rule;
 import org.junit.Test;
 
 import ch.epfl.sweng.SDP.R;
 import ch.epfl.sweng.SDP.auth.Account;
+import ch.epfl.sweng.SDP.firebase.Database;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -21,24 +24,29 @@ import static ch.epfl.sweng.SDP.game.WaitingPageActivityTest.waitForVisibility;
 
 public class ShopActivityTest {
 
+    private static String USER_ID = "123456789";
+    private static DatabaseReference usersRef = Database.getReference("users."
+                                                    + USER_ID + ".boughtItems");
+
     @Rule
     public final ActivityTestRule<ShopActivity> mActivityRule =
             new ActivityTestRule<ShopActivity>(ShopActivity.class) {
                 @Override
                 protected void beforeActivityLaunched() {
                     ShopActivity.disableAnimations();
+                    usersRef.removeValue();
                 }
             };
 
     @Test
     public void testPressItemAndCancel() {
-        SystemClock.sleep(8000);
+        SystemClock.sleep(5000);
         LinearLayout layout = mActivityRule.getActivity().findViewById(R.id.shopItems);
         LinearLayout layoutChild = (LinearLayout) layout.getChildAt(0);
-        //int id = View.generateViewId();
-        layoutChild.setId(View.generateViewId());
+        int id = View.generateViewId();
+        layoutChild.setId(id);
 
-        onView(withId(layoutChild.getId())).perform(click());
+        onView(withId(id)).perform(click());
         onView(withId(R.id.cancelButton)).perform(click());
         onView(withId(R.id.buyButton)).check(doesNotExist());
     }
@@ -46,7 +54,7 @@ public class ShopActivityTest {
     @Test
     public void testPressBuyItemNoStars() {
         //waitForVisibility(mActivityRule.getActivity().findViewById(R.id.shopItems), View.VISIBLE);
-        SystemClock.sleep(8000);
+        SystemClock.sleep(5000);
         LinearLayout layout = mActivityRule.getActivity().findViewById(R.id.shopItems);
         LinearLayout layoutChild = (LinearLayout) layout.getChildAt(0);
         int id = View.generateViewId();
@@ -65,7 +73,7 @@ public class ShopActivityTest {
     @Test
     public void testPressBuyItemSuccess() {
         Account.getInstance(mActivityRule.getActivity().getApplicationContext()).setStars(200);
-        SystemClock.sleep(8000);
+        SystemClock.sleep(5000);
         //waitForVisibility(mActivityRule.getActivity().findViewById(R.id.shopItems), View.VISIBLE);
         LinearLayout layout = mActivityRule.getActivity().findViewById(R.id.shopItems);
         LinearLayout layoutChild = (LinearLayout) layout.getChildAt(0);

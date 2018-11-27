d6f3fb6fe378b5903b57327151c0215c98a3c0e2
Claudio5 <claudio.loureiro1@gmail.com>
2018-11-27 21:12:35 +0100

    This one may pass

--- app/src/androidTest/java/ch/epfl/sweng/SDP/shop/ShopActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/shop/ShopActivityTest.java
@@ -1,20 +1,26 @@
 package ch.epfl.sweng.SDP.shop;
 
+import android.content.Context;
 import android.os.SystemClock;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import org.junit.After;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import ch.epfl.sweng.SDP.R;
 import ch.epfl.sweng.SDP.auth.Account;
+import ch.epfl.sweng.SDP.auth.ConstantsWrapper;
 import ch.epfl.sweng.SDP.firebase.Database;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -23,6 +29,10 @@ import static android.support.test.espresso.assertion.ViewAssertions.doesNotExis
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(AndroidJUnit4.class)
 public class ShopActivityTest {
@@ -31,6 +41,13 @@ public class ShopActivityTest {
     private static DatabaseReference usersRef = Database.getReference("users."
                                                     + USER_ID + ".boughtItems");
 
+    private static final String TEST_EMAIL = "testEmail";
+    private static final String USERNAME = "username";
+
+    private static ConstantsWrapper mockConstantsWrapper;
+    private static Account mockAccount;
+    private static Account account;
+
     @Rule
     public final ActivityTestRule<ShopActivity> mActivityRule =
             new ActivityTestRule<ShopActivity>(ShopActivity.class) {
@@ -38,9 +55,44 @@ public class ShopActivityTest {
                 protected void beforeActivityLaunched() {
                     ShopActivity.disableAnimations();
                     usersRef.removeValue();
-                    ShopActivity.enableTesting();
+                    //ShopActivity.enableTesting();
                 }
             };
+
+    @BeforeClass
+    public static void setup() {
+        mockConstantsWrapper = mock(ConstantsWrapper.class);
+        mockAccount = mock(Account.class);
+        DatabaseReference mockReference = mock(DatabaseReference.class);
+        Query mockQuery = mock(Query.class);
+
+        when(mockConstantsWrapper.getReference(isA(String.class))).thenReturn(mockReference);
+        when(mockConstantsWrapper.getFirebaseUserId()).thenReturn(USER_ID);
+
+        when(mockReference.child(isA(String.class))).thenReturn(mockReference);
+        when(mockReference.orderByChild(isA(String.class))).thenReturn(mockQuery);
+
+        when(mockQuery.equalTo(isA(String.class))).thenReturn(mockQuery);
+
+        doNothing().when(mockReference)
+                .setValue(isA(Integer.class), isA(DatabaseReference.CompletionListener.class));
+        doNothing().when(mockReference)
+                .setValue(isA(Boolean.class), isA(DatabaseReference.CompletionListener.class));
+        doNothing().when(mockReference)
+                .removeValue(isA(DatabaseReference.CompletionListener.class));
+
+        doNothing().when(mockQuery).addListenerForSingleValueEvent(isA(ValueEventListener.class));
+
+        doNothing().when(mockAccount).updateUsername(isA(String.class));
+        
+        Context context = InstrumentationRegistry.getContext();
+
+        Account.createAccount(context, mockConstantsWrapper, USERNAME,
+                TEST_EMAIL);
+        account = Account.getInstance(context);
+        account.setUserId(USER_ID);
+
+    }
 
     @After
     public void afterEachTest() {

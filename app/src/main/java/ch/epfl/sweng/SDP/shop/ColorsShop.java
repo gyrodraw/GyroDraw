e725eafe767602628d1700f90a35d4dc463b213a
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-11-30 11:27:15 +0100

    Change exception thrown to IllegalArgumentException and add message

--- app/src/main/java/ch/epfl/sweng/SDP/shop/ColorsShop.java
+++ app/src/main/java/ch/epfl/sweng/SDP/shop/ColorsShop.java
@@ -24,7 +24,7 @@ public enum ColorsShop {
             case "GREEN":
                 return GREEN;
             default:
-                throw new IllegalStateException();
+                throw new IllegalArgumentException(color + " not found");
         }
     }
 

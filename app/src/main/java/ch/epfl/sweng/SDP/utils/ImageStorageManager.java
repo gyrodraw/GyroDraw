d7c5a8a51a58385da77c0345777187b7d3cdf195
Fredrik Bixo <fredrik.bixo@epfl.ch>
2018-12-02 18:06:42 +0100

    Add documentation

--- app/src/main/java/ch/epfl/sweng/SDP/utils/ImageStorageManager.java
+++ app/src/main/java/ch/epfl/sweng/SDP/utils/ImageStorageManager.java
@@ -11,21 +11,28 @@ import java.io.FileOutputStream;
 
 public class ImageStorageManager {
 
-    // MARK: Saving
-    // TODO:add storge permission check
-    public static void saveImage(Bitmap finalBitmap, String image_name, Context context) {
+    /**
+     * Saves an image to the device file system.
+     * @param image the image to save.
+     * @param image_name the filename of the image.
+     * @param context the context.
+     */
+    public static void saveImage(Bitmap image, String image_name, Context context) {
 
+        // Get image dirctory
         String root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString()+ "/Camera/Your_Directory_Name";
         File myDir = new File(root);
         myDir.mkdirs();
         String fname = "Image-" + image_name + ".png";
         File file = new File(myDir, fname);
-        System.out.println(file.getAbsolutePath());
+        Log.d("ImageStorageManager",  "Saving image: " + root + fname);
+
         if (file.exists()) file.delete();
-        Log.i("LOAD", root + fname);
+
+        // Save image in file directory
         try {
             FileOutputStream out = new FileOutputStream(file);
-            finalBitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+            image.compress(Bitmap.CompressFormat.PNG, 90, out);
             out.flush();
             out.close();
         } catch (Exception e) {

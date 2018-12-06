2f5ca304cfebce171467f1e91153e2f29e95aa2a
Rafael Bischof <rafael.bischof@outlook.com>
2018-12-06 10:48:11 +0100

    Fixed failing test

--- app/src/androidTest/java/ch/epfl/sweng/SDP/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/MainActivityTest.java
@@ -2,6 +2,7 @@ package ch.epfl.sweng.SDP;
 
 import android.app.Activity;
 import android.app.Instrumentation;
+import android.content.Context;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -11,16 +12,21 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.internal.verification.checkers.AtLeastXNumberOfInvocationsChecker;
 
 import java.util.HashMap;
 
+import ch.epfl.sweng.SDP.auth.Account;
+import ch.epfl.sweng.SDP.auth.ConstantsWrapper;
 import ch.epfl.sweng.SDP.auth.LoginActivity;
+import ch.epfl.sweng.SDP.localDatabase.LocalDbHandlerForAccount;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
@@ -47,9 +53,53 @@ public class MainActivityTest {
 
     @Test
     public void testCloneAccountFromFirebase() {
+        HashMap<String,Object> values = new HashMap<>();
+        initializeAccountHashMap(values);
+
+        HashMap<String, HashMap<String, Object>> account = new HashMap<>();
+        account.put("no_user", values);
+
         DataSnapshot snapshot = Mockito.mock(DataSnapshot.class);
-        when(snapshot.getValue()).thenReturn(new HashMap<String, HashMap<String, Object>>());
+        when(snapshot.getValue()).thenReturn(account);
+
         mActivityRule.getActivity().cloneAccountFromFirebase(snapshot);
+
+        Account.deleteAccount();
+        Context context = mActivityRule.getActivity().getApplicationContext();
+        LocalDbHandlerForAccount localDbHandlerForAccount = new LocalDbHandlerForAccount(
+                context, null, 1);
+        Account.createAccount(context, new ConstantsWrapper(),
+                "TESTUSERNAME", "testEmail");
+        localDbHandlerForAccount.retrieveAccount(Account.getInstance(context));
+        Account newAccount = Account.getInstance(context);
+
+        assertThatAccountWasInitializedCorrectly(newAccount);
+    }
+
+    public void initializeAccountHashMap(HashMap<String, Object> values) {
+        values.put("username", "TESTUSERNAME");
+        values.put("userId", "no_user");
+        values.put("trophies", new Long(10));
+        values.put("stars", new Long(20));
+        values.put("matchesWon", new Long(30));
+        values.put("totalMatches", new Long(40));
+        values.put("averageRating", new Double(3.5));
+        values.put("email", "testEmail");
+        values.put("currentLeague", "leagueOne");
+        values.put("maxTrophies", new Long(100));
+    }
+
+    public void assertThatAccountWasInitializedCorrectly(Account newAccount) {
+        assertThat(newAccount.getUserId(), is(equalTo("no_user")));
+        assertThat(newAccount.getUsername(), is(equalTo("TESTUSERNAME")));
+        assertThat(newAccount.getTrophies(), is(10));
+        assertThat(newAccount.getStars(), is(20));
+        assertThat(newAccount.getMatchesWon(), is(30));
+        assertThat(newAccount.getTotalMatches(), is(40));
+        assertThat(newAccount.getAverageRating(), is(3.5));
+        assertThat(newAccount.getEmail(), is(equalTo("testEmail")));
+        assertThat(newAccount.getCurrentLeague(), is(equalTo("leagueOne")));
+        assertThat(newAccount.getMaxTrophies(), is(100));
     }
 
 }

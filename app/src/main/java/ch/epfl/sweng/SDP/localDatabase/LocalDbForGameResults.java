efb12bf243ec5b811d434887e8bf9f40064b0fdd
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-12-12 18:26:35 +0100

    Create interfaces for LocalDb classes

---
+++ app/src/main/java/ch/epfl/sweng/SDP/localDatabase/LocalDbForGameResults.java
@@ -0,0 +1,26 @@
+package ch.epfl.sweng.SDP.localDatabase;
+
+import android.content.Context;
+import ch.epfl.sweng.SDP.home.GameResult;
+import java.util.List;
+
+/**
+ * Interface representing a generic handler for the local database, responsible of operations
+ * involving {@link GameResult}.
+ */
+public interface LocalDbForGameResults {
+
+    /**
+     * Adds a game result to the local db.
+     *
+     * @param gameResult to insert
+     */
+    void addGameResultToDb(GameResult gameResult);
+
+    /**
+     * Retrieves the 10th most recent game results from the table.
+     *
+     * @return the newest game result
+     */
+    List<GameResult> getGameResultsFromDb(Context context);
+}

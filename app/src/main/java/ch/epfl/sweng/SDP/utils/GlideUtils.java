e712c3a957f36a2ca39d3cc3cabf8d15f128bbca
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-12-12 17:09:57 +0100

    Add javadocs and change signatures

--- app/src/main/java/ch/epfl/sweng/SDP/utils/GlideUtils.java
+++ app/src/main/java/ch/epfl/sweng/SDP/utils/GlideUtils.java
@@ -5,21 +5,41 @@ import ch.epfl.sweng.SDP.BaseActivity;
 import ch.epfl.sweng.SDP.R;
 import com.bumptech.glide.Glide;
 
+/**
+ * Utility class used to display animations using Glide.
+ */
 public final class GlideUtils {
-    private GlideUtils() {}
 
-    public static void startBackgroundAnimation(BaseActivity activity, int resId) {
+    private GlideUtils() {
+    }
+
+    /**
+     * Starts the background animation in the given activity.
+     *
+     * @param activity the activity in which the animation should be displayed
+     */
+    public static void startBackgroundAnimation(BaseActivity activity) {
         Glide.with(activity).load(R.drawable.background_animation)
-                .into((ImageView) activity.findViewById(resId));
+                .into((ImageView) activity.findViewById(R.id.backgroundAnimation));
     }
 
-    public static void startDotsWaitingAnimation(BaseActivity activity, int resId) {
+    /**
+     * Starts the dots waiting animation in the given activity.
+     *
+     * @param activity the activity in which the animation should be displayed
+     */
+    public static void startDotsWaitingAnimation(BaseActivity activity) {
         Glide.with(activity).load(R.drawable.waiting_animation_dots)
-                .into((ImageView) activity.findViewById(resId));
+                .into((ImageView) activity.findViewById(R.id.waitingAnimationDots));
     }
 
-    public static void startSquareWaitingAnimation(BaseActivity activity, int resId) {
+    /**
+     * Starts the square waiting animation in the given activity.
+     *
+     * @param activity the activity in which the animation should be displayed
+     */
+    public static void startSquareWaitingAnimation(BaseActivity activity) {
         Glide.with(activity).load(R.drawable.waiting_animation_square)
-                .into((ImageView) activity.findViewById(resId));
+                .into((ImageView) activity.findViewById(R.id.waitingAnimationSquare));
     }
 }

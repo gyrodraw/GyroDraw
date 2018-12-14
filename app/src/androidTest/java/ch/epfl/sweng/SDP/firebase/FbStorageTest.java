20edfb52b5a0b206b424f2c871e76c7610bf6c86
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-12-14 18:57:46 +0100

    Move FbStorageUnitTest to FbStorageTest and change them

---
+++ app/src/androidTest/java/ch/epfl/sweng/SDP/firebase/FbStorageTest.java
@@ -0,0 +1,22 @@
+package ch.epfl.sweng.SDP.firebase;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import android.graphics.Bitmap;
+import android.support.test.runner.AndroidJUnit4;
+import com.google.firebase.storage.StorageTask;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class FbStorageTest {
+
+    @Test
+    public void testSendBitmapToStorage() {
+        StorageTask task = FbStorage.sendBitmapToFirebaseStorage(Bitmap.createBitmap(2, 2,
+                Bitmap.Config.ARGB_8888), "testImage");
+        assertThat(task, is(notNullValue()));
+    }
+}

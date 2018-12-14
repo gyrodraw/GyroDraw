94dd83524da3c6c01186c8ed9976a1250b4f6ff1
Andrea Veneziano <andrea.veneziano@epfl.ch>
2018-12-14 18:41:14 +0100

    Add javadocs and change exception to IllegalArgumentException

--- app/src/main/java/ch/epfl/sweng/SDP/firebase/AccountAttributes.java
+++ app/src/main/java/ch/epfl/sweng/SDP/firebase/AccountAttributes.java
@@ -1,9 +1,21 @@
 package ch.epfl.sweng.SDP.firebase;
 
+import ch.epfl.sweng.SDP.auth.Account;
+
+/**
+ * Enum representing all the {@link Account}'s attributes.
+ */
 public enum AccountAttributes {
     USER_ID, USERNAME, EMAIL, STARS, TROPHIES, LEAGUE, MATCHES_WON, MATCHES_TOTAL,
     AVERAGE_RATING, MAX_TROPHIES, FRIENDS, STATUS, BOUGHT_ITEMS;
 
+    /**
+     * Converts the given {@link AccountAttributes} to a string which can be used in a path (for a
+     * Firebase Database query, for example).
+     *
+     * @param accountAttribute the account attribute to convert
+     * @return a string representing the given attribute
+     */
     static String attributeToPath(AccountAttributes accountAttribute) {
         switch (accountAttribute) {
             case USER_ID:
@@ -33,7 +45,7 @@ public enum AccountAttributes {
             case BOUGHT_ITEMS:
                 return "boughtItems";
             default:
-                throw new IllegalStateException("Unknown attribute");
+                throw new IllegalArgumentException("Unknown attribute");
         }
     }
 }

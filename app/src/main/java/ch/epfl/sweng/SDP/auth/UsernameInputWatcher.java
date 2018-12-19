116d348f303074ddc919b6e6ad5ad7824cf727c2
Rafael Bischof <rafael.bischof@outlook.com>
2018-12-19 13:50:31 +0100

    Added points to illegal chars

--- app/src/main/java/ch/epfl/sweng/SDP/auth/UsernameInputWatcher.java
+++ app/src/main/java/ch/epfl/sweng/SDP/auth/UsernameInputWatcher.java
@@ -1,19 +1,23 @@
 package ch.epfl.sweng.SDP.auth;
 
-import static java.lang.String.format;
-
 import android.content.res.Resources;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.TextView;
+
 import ch.epfl.sweng.SDP.R;
+
+import static java.lang.String.format;
 
 /**
  * This class is a listener which can be applied to the username input
  * box to check if the entered username contains problematic chars or not.
  */
 final class UsernameInputWatcher implements TextWatcher {
+
+    private static final int MIN_CHARS = 3;
+    private static final int MAX_CHARS = 15;
 
     private final TextView feedback;
     private final Button createAccount;
@@ -58,14 +62,15 @@ final class UsernameInputWatcher implements TextWatcher {
     private boolean checkAll(String username) {
         return check(username != null, R.string.usernameMustNotBeEmpty, "")
                 && check(!username.isEmpty(), R.string.usernameMustNotBeEmpty, "")
-                && check(username.length() >= 5, R.string.usernameTooShort, "")
-                && check(username.length() <= 20, R.string.usernameTooLong, "")
+                && check(username.length() >= MIN_CHARS, R.string.usernameTooShort, "")
+                && check(username.length() <= MAX_CHARS, R.string.usernameTooLong, "")
                 && check(!username.contains("  "),
                 R.string.usernameIllegalChar, " double spaces")
                 && check(!username.contains("\\"), R.string.usernameIllegalChar, " \\")
                 && check(!username.contains("%"), R.string.usernameIllegalChar, " %")
                 && check(!username.contains("\""), R.string.usernameIllegalChar, " \"")
-                && check(!username.contains("'"), R.string.usernameIllegalChar, " '");
+                && check(!username.contains("'"), R.string.usernameIllegalChar, " '")
+                && check(!username.contains("."), R.string.usernameIllegalChar, " points");
     }
 
     private void enableButton(boolean enable, int colorId) {

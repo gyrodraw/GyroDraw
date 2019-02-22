c3e14dfd3e2248cf53148062510114525fd32121
LudoHoff <ludo.hoffstetter@epfl.ch>
2019-02-22 11:26:00 +0100

    Fix online layout

--- app/src/main/java/ch/epfl/sweng/GyroDraw/home/leaderboard/Player.java
+++ app/src/main/java/ch/epfl/sweng/GyroDraw/home/leaderboard/Player.java
@@ -1,5 +1,27 @@
 package ch.epfl.sweng.GyroDraw.home.leaderboard;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+
+import java.util.LinkedList;
+
+import ch.epfl.sweng.GyroDraw.R;
+import ch.epfl.sweng.GyroDraw.auth.Account;
+import ch.epfl.sweng.GyroDraw.firebase.AccountAttributes;
+import ch.epfl.sweng.GyroDraw.firebase.FbDatabase;
+import ch.epfl.sweng.GyroDraw.firebase.OnSuccessValueEventListener;
+import ch.epfl.sweng.GyroDraw.utils.OnlineStatus;
+import ch.epfl.sweng.GyroDraw.utils.TestUsers;
+import ch.epfl.sweng.GyroDraw.utils.TypefaceLibrary;
+
 import static android.view.View.GONE;
 import static android.view.View.VISIBLE;
 import static ch.epfl.sweng.GyroDraw.firebase.AccountAttributes.LEAGUE;
@@ -9,25 +31,6 @@ import static ch.epfl.sweng.GyroDraw.firebase.AccountAttributes.USER_ID;
 import static ch.epfl.sweng.GyroDraw.home.FriendsRequestState.FRIENDS;
 import static ch.epfl.sweng.GyroDraw.home.FriendsRequestState.fromInteger;
 import static ch.epfl.sweng.GyroDraw.utils.LayoutUtils.getLeagueImageId;
-
-import android.content.Context;
-import android.content.res.Resources;
-import android.support.annotation.NonNull;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import ch.epfl.sweng.GyroDraw.R;
-import ch.epfl.sweng.GyroDraw.auth.Account;
-import ch.epfl.sweng.GyroDraw.firebase.AccountAttributes;
-import ch.epfl.sweng.GyroDraw.firebase.FbDatabase;
-import ch.epfl.sweng.GyroDraw.firebase.OnSuccessValueEventListener;
-import ch.epfl.sweng.GyroDraw.utils.OnlineStatus;
-import ch.epfl.sweng.GyroDraw.utils.TestUsers;
-import ch.epfl.sweng.GyroDraw.utils.TypefaceLibrary;
-import com.google.firebase.database.DataSnapshot;
-import java.util.LinkedList;
 
 /**
  * Helper class to manage and display user data from Firebase. Needs to be package-private.
@@ -44,7 +47,7 @@ class Player implements Comparable {
     private final boolean isCurrentUser;
 
     Player(Context context, String userId, String username, Long trophies, String league,
-            boolean isFriend, boolean isCurrentUser) {
+           boolean isFriend, boolean isCurrentUser) {
         this.context = context;
         this.userId = userId;
         this.username = username;
@@ -94,14 +97,14 @@ class Player implements Comparable {
         Resources res = context.getResources();
         styleView(usernameView, rank + ". " + username, res.getColor(
                 isCurrentUser ? R.color.colorPrimaryDark : R.color.colorDrawYellow),
-                new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 4));
+                new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 5));
         usernameView.setPadding(0, 10, 0, 10);
 
         TextView trophiesView = new TextView(context);
         styleView(trophiesView, trophies.toString(),
                 res.getColor(R.color.colorPrimaryDark),
                 new LinearLayout.LayoutParams(0,
-                        LinearLayout.LayoutParams.WRAP_CONTENT, 2));
+                        LinearLayout.LayoutParams.WRAP_CONTENT, 1));
 
         trophiesView.setTextAlignment(RelativeLayout.TEXT_ALIGNMENT_TEXT_END);
 
@@ -117,7 +120,7 @@ class Player implements Comparable {
                 new View[]{usernameView, trophiesView, leagueView, friendsButton});
 
         if (isFriend && !isCurrentUser) {
-            createAndAddOnlineView(res, entry);
+            createAndAddOnlineView(res, entry, usernameView);
         }
 
         entry.setBackgroundColor(res.getColor(
@@ -127,24 +130,28 @@ class Player implements Comparable {
         return entry;
     }
 
-    private void createAndAddOnlineView(Resources res, LinearLayout entry) {
-        final TextView onlineView = new TextView(context);
-        styleView(onlineView, "Online", res.getColor(R.color.colorGreen),
-                new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 2));
-        onlineView.setPadding(0, 10, 0, 10);
-        onlineView.setVisibility(GONE);
+    private void createAndAddOnlineView(final Resources res, final LinearLayout entry, final TextView usernameView) {
         FbDatabase.getUserOnlineStatus(userId, new OnSuccessValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Integer data = dataSnapshot.getValue(Integer.class);
                 if (data != null) {
                     OnlineStatus onlineStatus = OnlineStatus.fromInteger(data);
-                    onlineView.setVisibility(
-                            onlineStatus == OnlineStatus.ONLINE ? VISIBLE : GONE);
+                    if (onlineStatus == OnlineStatus.ONLINE) {
+                        final TextView onlineView = new TextView(context);
+                        styleView(onlineView, "O", res.getColor(R.color.colorGreen),
+                                new LinearLayout.LayoutParams(0,
+                                        LinearLayout.LayoutParams.WRAP_CONTENT, 1));
+                        onlineView.setTextAlignment(RelativeLayout.TEXT_ALIGNMENT_TEXT_END);
+                        usernameView.setLayoutParams(
+                                new LinearLayout.LayoutParams(0,
+                                        LinearLayout.LayoutParams.WRAP_CONTENT, 4));
+                        onlineView.setPadding(0, 10, 0, 10);
+                        entry.addView(onlineView, 1);
+                    }
                 }
             }
         });
-        entry.addView(onlineView, 1);
     }
 
     private LinearLayout addViews(LinearLayout layout, View[] views) {
@@ -155,7 +162,7 @@ class Player implements Comparable {
     }
 
     private void styleView(TextView view, String text, int color,
-            LinearLayout.LayoutParams layoutParams) {
+                           LinearLayout.LayoutParams layoutParams) {
         view.setText(text);
         view.setTextSize(20);
         view.setMaxLines(1);
@@ -175,7 +182,7 @@ class Player implements Comparable {
      * @param snapshot to convert
      */
     static void convertSnapshotToPlayerAndAddToList(Context context, DataSnapshot snapshot,
-            LinkedList<Player> players) {
+                                                    LinkedList<Player> players) {
         String userId = snapshot.child(USER_ID).getValue(String.class);
         String username = snapshot.child(USERNAME).getValue(String.class);
         Long trophies = snapshot.child(TROPHIES).getValue(Long.class);
